2d624c5ec8a4923a5c1cd4b50d170bd2e87d1312
Tamirlan Mamutov <tm1722@vertex22.doc.ic.ac.uk>
2024-01-24 15:03:33 +0000

    [tm1722][dk2722] Attempted lexer implementation (included identifiers, escapeDigit)

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -4,9 +4,27 @@ import parsley.Parsley
 import parsley.token.Lexer
 import parsley.token.descriptions._
 
+import parsley.token.predicate._
+import parsley.token.text.Escape
+import parsley.token.descriptions.text.EscapeDesc
+
 object lexer {
     private val desc = LexicalDesc.plain.copy(
         // your configuration goes here
+        nameDesc = NameDesc.plain.copy(
+            identifierStart = predicate.Basic(_.isLetter), // check for "_"
+            idetifierLetter = predicate.Basic(_.isLetterOrDigit), // check for "_"
+        ),
+        symbolDesc = SymbolDesc.plain,
+        numericDesc = NumericDesc.plain,
+        textDesc = TextDesc.plain.copy(
+            escapeSequences = new EscapeDesc.plain.copy(
+                escBegin = '\\',
+                literals = Set("0","b","t","n","f","r",'"',"'","\\"),
+                
+            ),
+        ),
+        spaceDesc = SpaceDesc.plain,
     )
     private val lexer = new Lexer(desc)
 
