2030f2a7389cddae5fa900db9437bde138826163
dk2722 <dk2722@ic.ac.uk>
2024-01-26 13:08:06 +0000

    [dk2722][tm1722] Initial implementation of lexer.

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -7,24 +7,39 @@ import parsley.token.descriptions._
 import parsley.token.predicate._
 import parsley.token.text.Escape
 import parsley.token.descriptions.text.EscapeDesc
+import parsley.token.descriptions.numeric.NumericDesc
+import parsley.token.descriptions.text.TextDesc
 
 object lexer {
     private val desc = LexicalDesc.plain.copy(
-        // your configuration goes here
         nameDesc = NameDesc.plain.copy(
-            identifierStart = predicate.Basic(_.isLetter), // check for "_"
-            idetifierLetter = predicate.Basic(_.isLetterOrDigit), // check for "_"
+            identifierStart = Basic(c => c.isLetter || c == '_'),
+            identifierLetter = Basic(c => c.isLetterOrDigit || c == '_'),
         ),
-        symbolDesc = SymbolDesc.plain,
-        numericDesc = NumericDesc.plain,
-        textDesc = TextDesc.plain.copy(
-            escapeSequences = new EscapeDesc.plain.copy(
-                escBegin = '\\',
-                literals = Set("0","b","t","n","f","r",'"',"'","\\"),
-                
+
+        symbolDesc = SymbolDesc.plain.copy(
+            hardKeywords = Set(
+                "begin", "end", "skip", "read", "free", "return", "exit", 
+                "print", "println", "if", "then", "else", "fi", "while", "do", "done", "fst", 
+                "snd", "int", "bool", "char", "string", "pair", "null", "true", "false"
             ),
         ),
-        spaceDesc = SpaceDesc.plain,
+
+        numericDesc = NumericDesc.plain,
+
+        textDesc = TextDesc.plain.copy(
+            escapeSequences = EscapeDesc.plain.copy(
+                escBegin = '\\',
+                mapping = Map("0" -> 0x0, "b" -> 0x8, "t" -> 0x9, "n" -> 0xa, "f" -> 0xc, "r" -> 0xd, "\"" -> 0x22, "\'" -> 0x27, "\\" -> 0x5c),
+            ),
+            characterLiteralEnd = '\'',
+            stringEnds = Set(("\"", "\"")),
+            graphicCharacter = Basic(c => c != '\\' && c != '\'' && c != '"' && c >= ' '),
+        ),
+
+        spaceDesc = SpaceDesc.plain.copy(
+            lineCommentStart = "#"
+        ),
     )
     private val lexer = new Lexer(desc)
 
