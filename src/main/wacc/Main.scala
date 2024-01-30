18e5e2fb7a28b70109bd57e5ece603475dca313a
Tamirlan Mamutov <tm1722@ash40.doc.ic.ac.uk>
2024-01-30 16:53:15 +0000

    [tm1722] Main now takes file path as an argument

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -5,13 +5,20 @@ import parsley.{Success, Failure}
 object Main {
     def main(args: Array[String]): Unit = {
         println("hello WACC!")
+        
+        if (args.length < 1) {
+            println("Please provide a file path as an argument")
+            return
+        }
 
-        args.headOption match {
-            case Some(expr) => parser.parse(expr) match {
+        val source = scala.io.Source.fromFile(args(0))
+        val lines = try source.mkString finally source.close()
+        lines.headOption match {
+            case Some(expr) => parser.parse(expr.toString()) match {
                 case Success(x) => println(s"$expr = $x")
                 case Failure(msg) => println(msg)
             }
-            case None => println("please enter an expression")
+            case None => println("File is empty")
         }
     }
 }
