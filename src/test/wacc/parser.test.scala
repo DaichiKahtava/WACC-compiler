5760a7dcf3c6848d793501f82e4cc4d27bd02532
Tamirlan Mamutov <tm1722@vertex02.doc.ic.ac.uk>
2024-02-02 13:09:10 +0000

    Added a parser test file

---
+++ src/test/wacc/parser.test.scala
@@ -0,0 +1,45 @@
+package wacc
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.BeforeAndAfterEach
+
+class parserTest extends AnyFlatSpec with BeforeAndAfterEach {
+
+    // "The funcEnd method" should "return true for Return and Exit statements" in {
+    //         val returnStmt = Return(expr)
+    //         val exitStmt = Exit(expr)
+    //         parser.funcEnd(returnStmt) shouldBe true
+    //         parser.funcEnd(exitStmt) shouldBe true
+    //     }
+        
+    // it should "return false for other statements" in {
+    //         val skipStmt = Skip
+    //         val declStmt = Decl(typep, ident, expr)
+    //         val asgnStmt = Asgn(lvalue, expr)
+    //         val readStmt = Read(lvalue)
+    //         val freeStmt = Free(expr)
+    //         val printStmt = Print(expr)
+    //         val printlnStmt = Println(expr)
+    //         val condStmt = Cond(expr, stmt1, stmt2)
+    //         val loopStmt = Loop(expr, stmt)
+    //         val bodyStmt = Body(stmt)
+    //         val delimitStmt = Delimit(stmt1, stmt2)
+
+    //         parser.funcEnd(skipStmt) shouldBe false
+    //         parser.funcEnd(declStmt) shouldBe false
+    //         parser.funcEnd(asgnStmt) shouldBe false
+    //         parser.funcEnd(readStmt) shouldBe false
+    //         parser.funcEnd(freeStmt) shouldBe false
+    //         parser.funcEnd(printStmt) shouldBe false
+    //         parser.funcEnd(printlnStmt) shouldBe false
+    //         parser.funcEnd(condStmt) shouldBe false
+    //         parser.funcEnd(loopStmt) shouldBe false
+    //         parser.funcEnd(bodyStmt) shouldBe false
+    //         parser.funcEnd(delimitStmt) shouldBe false
+    //     }
+
+
+
+
+}
