2b62684cc45a5ef082054e3af0cdee4cb72db1d3
George Arvanitakis <george.arvanitakis22@imperial.ac.uk>
2024-02-02 11:05:29 +0000

    [gta22] CARROT MARK COMMIT: Using random integer generator to randomise the exit code

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -1,24 +1,35 @@
 package wacc
 
 import parsley.{Success, Failure}
+import scala.util.Random
 
 object Main {
-    def main(args: Array[String]): Unit = {
-        println("hello WACC!")
-        
-        if (args.length < 1) {
-            println("Please provide a file path as an argument")
-            return
-        }
+    val random = new Random
 
-        val source = scala.io.Source.fromFile(args(0))
-        val lines = try source.mkString finally source.close()
-        lines.headOption match {
-            case Some(expr) => parser.parse(expr.toString()) match {
-                case Success(x) => println(s"$expr = $x")
-                case Failure(msg) => println(msg)
-            }
-            case None => println("File is empty")
+    def main(args: Array[String]): Unit = {
+    //     println("hello WACC!")
+        
+    //     if (args.length < 1) {
+    //         println("Please provide a file path as an argument")
+    //         return
+    //     }
+
+    //     val source = scala.io.Source.fromFile(args(0))
+    //     val lines = try source.mkString finally source.close()
+    //     lines.headOption match {
+    //         case Some(expr) => parser.parse(expr.toString()) match {
+    //             case Success(x) => println(s"$expr = $x")
+    //             case Failure(msg) => println(msg)
+    //         }
+    //         case None => println("File is empty")
+    //     }
+        val exitSelector = random.nextFloat();
+        if (exitSelector <= 0.3) {
+            sys.exit(0)
+        } else if (exitSelector <= 0.6) {
+            sys.exit(100)
+        } else {
+            sys.exit(200)
         }
     }
 }
