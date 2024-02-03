2c03a00c4914a68104953864f3360ad2987a318c
Tamirlan Mamutov <tm1722@texel38.doc.ic.ac.uk>
2024-02-03 14:55:56 +0000

    [tm1722] Implemented tests for funcEnd method

--- src/test/wacc/parser.test.scala
+++ src/test/wacc/parser.test.scala
@@ -6,38 +6,58 @@ import org.scalatest.BeforeAndAfterEach
 
 class parserTest extends AnyFlatSpec with BeforeAndAfterEach {
 
-    // "The funcEnd method" should "return true for Return and Exit statements" in {
-    //         val returnStmt = Return(expr)
-    //         val exitStmt = Exit(expr)
-    //         parser.funcEnd(returnStmt) shouldBe true
-    //         parser.funcEnd(exitStmt) shouldBe true
-    //     }
-        
-    // it should "return false for other statements" in {
-    //         val skipStmt = Skip
-    //         val declStmt = Decl(typep, ident, expr)
-    //         val asgnStmt = Asgn(lvalue, expr)
-    //         val readStmt = Read(lvalue)
-    //         val freeStmt = Free(expr)
-    //         val printStmt = Print(expr)
-    //         val printlnStmt = Println(expr)
-    //         val condStmt = Cond(expr, stmt1, stmt2)
-    //         val loopStmt = Loop(expr, stmt)
-    //         val bodyStmt = Body(stmt)
-    //         val delimitStmt = Delimit(stmt1, stmt2)
+    val testAtomExpr = IntL(0)
+    
 
-    //         parser.funcEnd(skipStmt) shouldBe false
-    //         parser.funcEnd(declStmt) shouldBe false
-    //         parser.funcEnd(asgnStmt) shouldBe false
-    //         parser.funcEnd(readStmt) shouldBe false
-    //         parser.funcEnd(freeStmt) shouldBe false
-    //         parser.funcEnd(printStmt) shouldBe false
-    //         parser.funcEnd(printlnStmt) shouldBe false
-    //         parser.funcEnd(condStmt) shouldBe false
-    //         parser.funcEnd(loopStmt) shouldBe false
-    //         parser.funcEnd(bodyStmt) shouldBe false
-    //         parser.funcEnd(delimitStmt) shouldBe false
-    //     }
+    "The funcEnd method" should "return true for Return and Exit statements" in {
+        parser.funcEnd(Return(testAtomExpr)) shouldBe true
+        parser.funcEnd(Exit(testAtomExpr)) shouldBe true
+    }
+
+    it should "return true for Cond statements with both of the results being exiting (also checks recursively)" in {
+        parser.funcEnd(Cond(testAtomExpr, Return(testAtomExpr), Return(testAtomExpr))) shouldBe true
+        parser.funcEnd(Cond(testAtomExpr, Exit(testAtomExpr), Exit(testAtomExpr))) shouldBe true
+        parser.funcEnd(Cond(testAtomExpr, Return(testAtomExpr), Exit(testAtomExpr))) shouldBe true
+        parser.funcEnd(Cond(testAtomExpr, Exit(testAtomExpr), Return(testAtomExpr))) shouldBe true
+        parser.funcEnd(Cond(testAtomExpr, Cond(testAtomExpr, Return(testAtomExpr), Return(testAtomExpr)), Return(testAtomExpr))) shouldBe true
+    }
+
+    it should "return false for Cond statements with at least 1 of the result NOT being exiting (also checks recursively)" in {
+        parser.funcEnd(Cond(testAtomExpr, Skip, Return(testAtomExpr))) shouldBe false
+        parser.funcEnd(Cond(testAtomExpr, Exit(testAtomExpr), Skip)) shouldBe false
+        parser.funcEnd(Cond(testAtomExpr, Skip, Skip)) shouldBe false
+        parser.funcEnd(Cond(testAtomExpr, Return(testAtomExpr), Cond(testAtomExpr, Skip, Return(testAtomExpr)))) shouldBe false
+    }
+        
+    it should "return true for Delimit statements with last statement being exiting (also checks recursively)" in {
+        parser.funcEnd(Delimit(Return(testAtomExpr), Return(testAtomExpr))) shouldBe true
+        parser.funcEnd(Delimit(Return(testAtomExpr), Exit(testAtomExpr))) shouldBe true
+        parser.funcEnd(Delimit(Skip, Return(testAtomExpr))) shouldBe true
+        parser.funcEnd(Delimit(Skip, Exit(testAtomExpr))) shouldBe true
+        parser.funcEnd(Delimit(Skip, Delimit(Skip, Exit(testAtomExpr)))) shouldBe true
+    }
+
+    it should "return false for Delimit statements with last statement NOT being exiting (also checks recursively)" in {
+        parser.funcEnd(Delimit(Return(testAtomExpr), Skip)) shouldBe false
+        parser.funcEnd(Delimit(Exit(testAtomExpr), Skip)) shouldBe false
+        parser.funcEnd(Delimit(Skip, Delimit(Exit(testAtomExpr), Skip))) shouldBe false
+    }
+
+    it should "return false for other statements" in {
+        val testLValue = LIdent("test")
+        val testIdent = "test"
+        val testType = IntT
+
+        parser.funcEnd(Skip) shouldBe false
+        parser.funcEnd(Decl(testType, testIdent, testAtomExpr)) shouldBe false
+        parser.funcEnd(Asgn(testLValue, testAtomExpr)) shouldBe false
+        parser.funcEnd(Read(testLValue)) shouldBe false
+        parser.funcEnd(Free(testAtomExpr)) shouldBe false
+        parser.funcEnd(Print(testAtomExpr)) shouldBe false
+        parser.funcEnd(Println(testAtomExpr)) shouldBe false
+        parser.funcEnd(Loop(testAtomExpr, Skip)) shouldBe false
+        parser.funcEnd(Body(Skip)) shouldBe false
+        }
 
 
 
