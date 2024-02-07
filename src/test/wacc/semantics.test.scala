a18d2793f0b00822f97e7af3204ac1254a459015
dk2722 <dk2722@ic.ac.uk>
2024-02-07 16:14:47 +0000

    [dk2722] Test skeletons for statements.

--- src/test/wacc/semantics.test.scala
+++ src/test/wacc/semantics.test.scala
@@ -5,7 +5,6 @@ import org.scalatest.matchers.should.Matchers._
 
 class semanticsTests extends AnyFlatSpec {
   // Tests for sem.getLowestCommonAncestor.
-  // Test case for an empty list.
 
   val sem = new Semantics("foo.txt")
 
@@ -174,14 +173,10 @@ class semanticsTests extends AnyFlatSpec {
   }
 
 
+  // 
 
-  "The semantic checker" should "unidirectionally allow char[] to be weakened to string" in {
-    val chArr = S_ARRAY(S_CHAR)
-    sem.canWeakenTo(chArr, S_STRING) shouldBe true
-    sem.canWeakenTo(S_STRING, chArr) shouldBe false
-  }
 
-  it should "maintain that the type parameters of pairs are invariant" in {
+  "Pair types" should "maintain that their type parameters are invariant" in {
     val p1 = S_PAIR(S_ARRAY(S_CHAR), S_INT)
     val p2 = S_PAIR(S_STRING, S_INT)
     val pAny = S_PAIR(S_ANY, S_ANY)
@@ -189,7 +184,7 @@ class semanticsTests extends AnyFlatSpec {
     sem.canWeakenTo(p1, pAny) shouldBe false 
   }
   
-  it should "allow erased pairs to be compatible with any pairs" in {
+  it should "should be fully coercible with erased pair types" in {
     val erasedPair = S_ERASED
     val p1 = S_PAIR(S_STRING, S_INT)
     val p2 = S_PAIR(S_ANY, S_ERASED)
@@ -199,13 +194,52 @@ class semanticsTests extends AnyFlatSpec {
     sem.canWeakenTo(erasedPair, p2) shouldBe true
   }
 
-  it should "maintain that the type parameters of arrays are invariant" in {
+  "Array types" should "maintain that the type parameters of arrays are invariant" in {
     val arr1 = S_ARRAY(S_ARRAY(S_CHAR))
     val arr2 = S_ARRAY(S_STRING)
     val arr3 = S_ARRAY(S_ANY)
     sem.canWeakenTo(arr1, arr2) shouldBe false
     sem.canWeakenTo(arr2, arr3) shouldBe false
+  } 
+  
+  it should "unidirectionally allow char[] to be weakened to string" in {
+    val chArr = S_ARRAY(S_CHAR)
+    sem.canWeakenTo(chArr, S_STRING) shouldBe true
+    sem.canWeakenTo(S_STRING, chArr) shouldBe false
   }
+
+  // Lots to check for this... probably unnecessary since these are trivial
+  "Operators" should "have the correct return types" in {
+    sem.isSemCorrect(Not(BoolL(true))) shouldBe true
+    sem.isSemCorrect(Not(IntL(1))) shouldBe false
+    sem.isSemCorrect(Not(CharL('a'))) shouldBe false
+  }
+
+  "Control flow statements" should "have a condition of type Bool" in {
+    
+  }
+
+  "Free statements" should "accept arguments of pair or array type" in {
+
+  }
+
+  "Exit statements" should "accept arguments of type int" in {
+
+  }
+
+  "Read statements" should "accept arguments of either type int or type char" in {
+
+  }
+
+  "Return statements" should "not be in the main body of the program" in {
+
+  }
+
+  it should "return a type that is compatible with the return type of the enclosing function" in {
+
+  }
+
+
 
   // TODO test for different namespaces for functoins and variables
 }
