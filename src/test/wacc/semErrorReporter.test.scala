2ccd982f6c9196bd7d1cc44023994e68f63fd3ea
George Arvanitakis <george.arvanitakis22@imperial.ac.uk>
2024-02-09 18:58:25 +0000

    [gta22] Improved error messages

--- src/test/wacc/semErrorReporter.test.scala
+++ src/test/wacc/semErrorReporter.test.scala
@@ -9,7 +9,7 @@ class semErrorReporterTest  extends AnyFlatSpec with BeforeAndAfterEach {
     "The semantic report tool" should "be able to handle non existing files" in {
         val er = new SemErrorReporter("foo.txt")
         er.printSourceSection((1, 1))
-        er.toString() shouldBe """In file: foo.txt at position (1, 1)
+        er.toString() shouldBe """Semantic error in file: foo.txt at position (line: 1, column: 1)
 *** Could not display the source file: foo.txt ***
 -----
 Found 0 semantic errors.
@@ -19,7 +19,7 @@ Found 0 semantic errors.
     it should "correctly display a sourcefile location" in {
         val er = new SemErrorReporter("compile")
         er.printSourceSection((4, 4))
-        er.toString() shouldBe """In file: compile at position (4, 4)
+        er.toString() shouldBe """Semantic error in file: compile at position (line: 4, column: 4)
 | # You are free to change the language used for this script,
 | # but do *not* change its name.
      ^
@@ -32,7 +32,7 @@ Found 0 semantic errors.
     it should "correctly display something in the first line" in {
         val er = new SemErrorReporter("compile")
         er.printSourceSection((1, 1))
-        er.toString() shouldBe """In file: compile at position (1, 1)
+        er.toString() shouldBe """Semantic error in file: compile at position (line: 1, column: 1)
 | #!/bin/bash
   ^
 | # Bash front-end for your compiler.
@@ -44,7 +44,7 @@ Found 0 semantic errors.
     it should "correctly display something in the last line" in {
         val er = new SemErrorReporter("compile")
         er.printSourceSection((9, 1))
-        er.toString() shouldBe """In file: compile at position (9, 1)
+        er.toString() shouldBe """Semantic error in file: compile at position (line: 9, column: 1)
 | 
 | exit $?
   ^
