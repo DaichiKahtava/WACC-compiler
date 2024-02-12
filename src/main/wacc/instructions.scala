c86b58f5c028b0b61ef02dd68f985d7fa3cb35b6
dk2722 <dk2722@ic.ac.uk>
2024-02-12 11:59:51 +0000

    [dk2722] Added more instructions to ADT.

--- src/main/wacc/instructions.scala
+++ src/main/wacc/instructions.scala
@@ -3,18 +3,27 @@ package wacc
 sealed trait Instruction
 
 case class Label(label: String) extends Instruction
-case class Jump(fName: String) extends Instruction
-case object Return extends Instruction
+case class Jump(label: String) extends Instruction
+case class Return(label: String) extends Instruction
 
-case class Move(dest: Operand, src: Operand) extends Instruction
+// Replace operand with more specific classes if necessary
+case class Move(src: Operand, dst: Operand) extends Instruction
+case class Load(src: Operand, dst: Operand) extends Instruction
+case class Store(src: Operand, dst: Operand) extends Instruction
+case class Address(src: Operand, dst: Operand) extends Instruction
+case class Branch(label: String) extends Instruction
+case class BranchL(label: String, addr: Int) extends Instruction // Calls function and stores address in the link register...
 
-case class Add(dest: Operand, src: Operand) extends Instruction
-case class Sub(dest: Operand, src: Operand) extends Instruction
-case class Mul(dest: Operand, src: Operand) extends Instruction
-case class Div(dest: Operand, src: Operand) extends Instruction
+// May want to have the two operands and the destination as separate arguments
+// case class Add(op1, op2, dst)
+case class Add(src: Operand, dst: Operand) extends Instruction
+case class Sub(src: Operand, dst: Operand) extends Instruction
+case class Mul(src: Operand, dst: Operand) extends Instruction
+case class Div(src: Operand, dst: Operand) extends Instruction
 
 case class Compare(r1: Operand, r2: Operand) extends Instruction
 
+// TODO: Fill in with all types of operands
 sealed trait Operand
-
-case class Register() extends Operand
+case class Register(addr: Int) extends Operand
+case class ImmNum(n: Int) extends Operand
