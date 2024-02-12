481e1b95b1c4a962fe4089d5f6b467ddfa2fbf76
Shaban Kashif <mk2520@imperial.ac.uk>
2024-02-12 15:38:25 +0000

    Attempted generateInstructionList for some expressions.

--- src/main/wacc/treeWalker.scala
+++ src/main/wacc/treeWalker.scala
@@ -4,9 +4,11 @@ package wacc
 class treeWalker(var curSymTable: SymTable) {
     // GLOBAL POINTER TO THE FINAL (NOT-FORMATTED) ASSEMBLY CODE
     var instructionList = List()
+    var nextTempReg = 1 // Start with R1 for temporary values?
 
     def generateInstructionList(e: Expr) : List[Instruction] = e match {
         // UnOp expressions.
+
         case Not(x) => List()
         case Neg(x) => List()
         case Len(x) => List()
@@ -14,11 +16,29 @@ class treeWalker(var curSymTable: SymTable) {
         case Chr(x) => List()
 
         // BinOp expressions.
+
         case Mul(x, y) => List()
         case Div(x, y) => List()
         case Mod(x, y) => List()
-        case Add(x, y) => List()
-        case Minus(x, y) => List()
+
+        // Pattern match for Add.
+        case Add(left, right) => 
+            val output = generateInstructionList(left) ++ // Evaluate the left-hand side first.
+            generateInstructionList(right) ++ 
+            List(AddI(Register(nextTempReg), Register(0))) // ADD, put result in R0.
+            val newTempReg = nextTempReg + 1
+            nextTempReg = newTempReg // Update your register reference.
+            return output
+        
+        // Similar to Add, replace AddI with SubI.
+        case Minus(left, right) =>
+            val output = generateInstructionList(left) ++ 
+            generateInstructionList(right) ++ 
+            List(SubI(Register(nextTempReg), Register(0)))
+            val newTempReg = nextTempReg + 1
+            nextTempReg = newTempReg
+            return output
+
         case GrT(x, y) => List()
         case GrEqT(x, y) => List()
         case LsT(x, y) => List()
@@ -29,16 +49,28 @@ class treeWalker(var curSymTable: SymTable) {
         case Or(x, y) => List()
 
         // Atom expressions.
-        case IntL(n) => List()
+
+        // Load the integer directly.
+        case IntL(n) => List(Load(ImmNum(n), Register(0)))
+        
         case BoolL(b) => List()
         case CharL(c) => List()
         case StrL(s) => List()
         case PairL() => List()
-        case Ident(id) => List()
+
+        // Pattern match for the identifier.
+        case Ident(id) =>
+            curSymTable.findVarGlobal(id) match {
+                // Variable was found in the symbol table.
+                case Some(varInfo) => List(Load(varInfo.asInstanceOf[Operand], Register(0)))
+                // Variable was not found in the symbol table.
+                case None => throw new RuntimeException(s"Undefined variable: $id")
+        }
+
         case ArrElem(id, xs) => List()
 
         // Defaulting case.
-        case _ => throw new RuntimeException(s"Undefined expression.")
+        case _ => throw new RuntimeException("Undefined expression.")
     }
 
     def generateInstructionList(list: List[Any]) : List[Instruction] = ???
