55a8a8752c905ded9809b3b2da398c8493d4a936
Shaban Kashif <mk2520@imperial.ac.uk>
2024-02-12 14:21:13 +0000

    Added default cases for expressions.

--- src/main/wacc/treeWalker.scala
+++ src/main/wacc/treeWalker.scala
@@ -5,7 +5,41 @@ class treeWalker(var curSymTable: SymTable) {
     // GLOBAL POINTER TO THE FINAL (NOT-FORMATTED) ASSEMBLY CODE
     var instructionList = List()
 
-    def generateInstructionList(e: Expr) : List[Instruction] = ???
+    def generateInstructionList(e: Expr) : List[Instruction] = e match {
+        // UnOp expressions.
+        case Not(x) => List()
+        case Neg(x) => List()
+        case Len(x) => List()
+        case Ord(x) => List()
+        case Chr(x) => List()
+
+        // BinOp expressions.
+        case Mul(x, y) => List()
+        case Div(x, y) => List()
+        case Mod(x, y) => List()
+        case Add(x, y) => List()
+        case Minus(x, y) => List()
+        case GrT(x, y) => List()
+        case GrEqT(x, y) => List()
+        case LsT(x, y) => List()
+        case LsEqT(x, y) => List()
+        case Eq(x, y) => List()
+        case NEq(x, y) => List()
+        case And(x, y) => List()
+        case Or(x, y) => List()
+
+        // Atom expressions.
+        case IntL(n) => List()
+        case BoolL(b) => List()
+        case CharL(c) => List()
+        case StrL(s) => List()
+        case PairL() => List()
+        case Ident(id) => List()
+        case ArrElem(id, xs) => List()
+
+        // Defaulting case.
+        case _ => throw new RuntimeException(s"Undefined expression.")
+    }
 
     def generateInstructionList(list: List[Any]) : List[Instruction] = ???
 
