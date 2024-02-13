bd9aaf0bda0d892b9cbc41a8da0f7a8d1e5fcdef
Tamirlan Mamutov <tm1722@texel19.doc.ic.ac.uk>
2024-02-13 16:59:18 +0000

    [tm1722] Made a tree walker test file

---
+++ src/test/wacc/treeWalker.test.scala
@@ -0,0 +1,17 @@
+package wacc
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.BeforeAndAfterEach
+
+class treeWalkerTest extends AnyFlatSpec with BeforeAndAfterEach
+{
+    // val test_tree_walker = new TreeWalker(new SymTable(None, None))
+
+    // "A tree-walker" should "generate the correct assembly code for begin end" in {
+    //     val test_ast = Program(List(),Exit(Neg(IntL(1))))(0,0)
+    //     val expected = List()
+    //     val result = test_tree_walker.generateInstructionList(test_ast)
+    //     result should be (expected)
+    // }
+}
